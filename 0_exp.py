6d8980f3ccf1e7793f50627a669f24dfaf51c011
Rone Chen <m13552079019@163.com>
2024-11-26 21:20:23 +0800

    change the coefficent range

--- 0_exp.py
+++ 0_exp.py
@@ -3,7 +3,7 @@ import os
 psnr_list = []
 ssim_list = []
 
-for coef in range(0, 64):
+for coef in range(0, 65):
     msg = os.popen(f'python main.py --input input.jpg --coeffs {coef}')
     output = msg.read()
     print(output, end='')
