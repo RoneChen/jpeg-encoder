53b78eda8b386aee503d4c8b7302ebb72ba262b3
Rone Chen <m13552079019@163.com>
2024-11-26 21:30:04 +0800

    update the coefficient and scale factor analysis

--- 0_exp.py
+++ 0_exp.py
@@ -1,11 +1,21 @@
 import os
 
-psnr_list = []
-ssim_list = []
 
-for coef in range(0, 65):
-    msg = os.popen(f'python main.py --input input.jpg --coeffs {coef}')
-    output = msg.read()
-    print(output, end='')
+def coefficient_analysis():
+    r'''
+    This function is used to analyze the coefficients of the DCT (Discrete Cosine Transform) in an image.
+    '''
+    for coef in range(0, 65):
+        msg = os.popen(f'python main.py --input input.jpg --coeffs {coef}')
+        output = msg.read()
+        print(output, end='')
 
-    
+def scale_factor_analysis():
+    r'''
+    This function is used to analyze the impact of the scale factor on the quality of the reconstructed image.
+    '''
+    for scale_factor in [1, 2, 4, 8]:
+        msg = os.popen(f'python main.py --input input.jpg --scale-factor {scale_factor}')
+        output = msg.read()
+        print(output, end='')
+        
